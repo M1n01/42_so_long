218dd66f70252558fa2da9ba17d0d97ba381f376
Minato Abe <minato.cat.love@gmail.com>
2023-05-02 17:10:38 +0900

    hyde debug.h

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:00:26 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/02 13:39:16 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/02 17:10:15 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include "libft.h"
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
-#include "debug.h"
+// #include "debug.h"
 
 typedef struct s_mlx
 {
