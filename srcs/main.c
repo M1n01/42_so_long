96efbdfcf3f54a7a846bae862392098ddc559e2e
Minato Abe <minato.cat.love@gmail.com>
2023-05-04 00:12:42 +0900

    want to run mlx

--- srcs/main.c
+++ srcs/main.c
@@ -6,52 +6,52 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 16:43:24 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/03 19:35:01 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/04 00:02:38 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	deal_key(int key, void *param)
-{
-	ft_putchar('X');
-	(void)key;
-	(void)param;
-	return (0);
-}
-
-int	main()
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-	mlx_key_hook(win_ptr, deal_key, NULL);
-	mlx_loop(mlx_ptr);
-	return (0);
-}
-
-// void	so_long(char *file);
-
-// int	main(int argc, char **argv)
+// int	deal_key(int key, void *param)
 // {
-// 	if (argc < 2)
-// 		ft_error("No map file");
-// 	so_long(argv[1]);
+// 	ft_putchar('X');
+// 	(void)key;
+// 	(void)param;
 // 	return (0);
 // }
 
-// void	so_long(char *file)
+// int	main()
 // {
-// 	char	*map;
+// 	void	*mlx_ptr;
+// 	void	*win_ptr;
 
-// 	map = get_map(file);
-// 	if (check_map(map) == false)
-// 		ft_error("Invalid map");
-// 	printf("[map]\n%s\n", map);
-// 	mlx(map);
-// 	ft_free(map);
-// 	return ;
+// 	mlx_ptr = mlx_init();
+// 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+// 	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+// 	mlx_key_hook(win_ptr, deal_key, NULL);
+// 	mlx_loop(mlx_ptr);
+// 	return (0);
 // }
+
+void	so_long(char *file);
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		ft_error("No map file");
+	so_long(argv[1]);
+	return (0);
+}
+
+void	so_long(char *file)
+{
+	char	*map;
+
+	map = get_map(file);
+	if (check_map(map) == false)
+		ft_error("Invalid map");
+	printf("[map]\n%s\n", map);
+	mlx(map);
+	ft_free(map);
+	return ;
+}
---
+++ srcs/map.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 15:44:43 by minabe            #+#    #+#             */
+/*   Updated: 2023/05/04 00:12:31 by minabe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+static size_t	count_map_width(char *map)
+{
+	size_t	i;
+	size_t	j;
+	size_t	width;
+
+	i = 0;
+	width = 0;
+	while (map[i] != '\0')
+	{
+		j = 0;
+		while (map[i+j] != '\n')
+			j++;
+		if (width == 0)
+			width = j;
+		else if (width != j)
+		{
+			free(map);
+			ft_error("Invalid map");
+		}
+		i += j + 1;
+	}
+	return (width);
+}
+
+static size_t	count_map_height(char *map)
+{
+	size_t	i;
+	size_t	height;
+
+	i = 0;
+	height = 1;
+	while (map[i] != '\0')
+	{
+		if (map[i] == '\n')
+		{
+			if (map[i + 1] == '\0')
+			{
+				ft_free(map);
+				ft_error("Invalid map");
+			}
+			height++;
+		}
+		i++;
+	}
+	return (height);
+}
+
+bool	check_obj(char *map)
+{
+	size_t	i;
+	size_t	start;
+	size_t	item;
+	size_t	goal;
+
+	i = 0;
+	start = 0;
+	item = 0;
+	goal = 0;
+	while (map[i] != '\0')
+	{
+		if (map[i] == 'P')
+			start++;
+		else if (map[i] == 'E')
+			goal++;
+		else if (map[i] == 'C')
+			item++;
+		else if (map[i] != '1' && map[i] != '0' && map[i] != '\n')
+			return (false);
+		i++;
+	}
+	if (start != 1 || goal != 1 || item == 0)
+		return (false);
+	return (true);
+}
+
+bool	check_map(char *map)
+{
+	if (count_map_width(map) == count_map_height(map))
+		return (false);
+	return (check_obj(map));
+}
+
+char	*get_map(char *file)
+{
+	int		fd;
+	char	*map;
+	char	*new;
+	char	*buf;
+	ssize_t	read_size;
+
+	map = ft_strdup(""); // malloc
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		ft_error("Failed to open file");
+	read_size = 1;
+	while (read_size > 0)
+	{
+		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // malloc
+		if (buf == NULL)
+			ft_error("malloc failed");
+		read_size = read(fd, buf, BUFFER_SIZE);
+		if (read_size < 0)
+			ft_error("Failed to read file");
+		buf[read_size] = '\0';
+		new = ft_strjoin(map, buf); // malloc
+		ft_free(buf); // free
+		ft_free(map); // free
+		map = new;
+	}
+	close(fd);
+	return (map);
+}
