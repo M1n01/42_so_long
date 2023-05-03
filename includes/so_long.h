a01054d6b0f2f43616b9e0cccc9b8e0977c408c7
Minato Abe <minato.cat.love@gmail.com>
2023-05-03 12:22:25 +0900

    check linux

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:00:26 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/02 21:54:37 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/03 12:22:22 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <unistd.h>
 # include "libft.h"
 # include "../minilibx-linux/mlx.h"
-// # include "../minilibx-linux/mlx_int.h"
+# include "../minilibx-linux/mlx_int.h"
 // #include "debug.h"
 
 typedef struct s_mlx
