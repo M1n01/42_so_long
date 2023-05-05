c00fac87e3a970ed74734ed0173f15fa8419a847
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 17:33:42 +0900

    not abort??

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:44:43 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/05 17:33:01 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 17:33:39 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,11 +121,11 @@ char	*get_map(char *file)
 		buf[read_size] = '\0';
 		tmp = ft_strjoin(map, buf);
 		printf("[tmp]\n%s\n", tmp);
-		// free(map);
 		map = tmp;
 	}
 	printf("[buf]: %zd\n%s\n", read_size, buf);
-	// free(buf);
+	free(map);
+	free(buf);
 	close(fd);
 	puts("close");
 	return (map);
