a326b5378c2ee6d9491c22332947ea99cee9a967
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 16:44:11 +0900

    not abort??

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:44:43 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/05 16:41:50 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 16:44:07 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,7 +114,7 @@ char	*get_map(char *file)
 	if (buf == NULL)
 		ft_error("malloc failed");
 	read_size = 1;
-	while (read_size > 0)
+	while (read_size != 0)
 	{
 		read_size = read(fd, buf, BUFFER_SIZE);
 		if (read_size < 0)
