3f12cd29726614e9676ee3099c08ed796d09ca30
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 22:19:01 +0900

    not abort??

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 17:17:04 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/05 22:13:08 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 22:18:58 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ char	*ft_strjoin(char *s1, char *s2)
 	str = calloc(len1 + len2 + 1, sizeof(char));
 	if (str == NULL)
 		ft_error("malloc failed");
-	memcpy(str, s1, len1);
-	memcpy(str + len1, s2, len2);
+	ft_memcpy(str, s1, len1);
+	ft_memcpy(str + len1, s2, len2);
 	str[len1 + len2] = '\0';
 	return (str);
 }
