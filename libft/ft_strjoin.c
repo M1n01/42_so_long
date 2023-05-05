28c487bc1800be7dd65d6ab918335a5d8db9407b
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 17:37:42 +0900

    not abort??

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 17:17:04 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/05 17:37:03 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 17:37:34 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,5 @@ char	*ft_strjoin(char *s1, char *s2)
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(str + len1, s2, len2);
 	str[len1 + len2] = '\0';
-	if (s1 != NULL)
-		ft_free(s1);
 	return (str);
 }
