01c734ce5c08c27424ac8d56d11104cc00a87980
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 17:09:10 +0900

    not abort??

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 15:44:43 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/05 17:06:18 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 17:09:06 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,8 +100,8 @@ bool	check_map(char *map)
 char	*get_map(char *file)
 {
 	int		fd;
-	char	*map;
-	char	*tmp;
+	// char	*map;
+	// char	*tmp;
 	char	*buf;
 	ssize_t	read_size;
 
@@ -112,19 +112,18 @@ char	*get_map(char *file)
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // malloc
 	if (buf == NULL)
 		ft_error("malloc failed");
-	read_size = 1;
-	while (read_size != 0)
-	{
-		read_size = read(fd, buf, BUFFER_SIZE);
-		if (read_size < 0)
-			ft_error("Failed to read file");
-		buf[read_size] = '\0';
-		tmp = ft_strjoin(map, buf);
-		ft_free(map);
-		map = tmp;
-	}
+	// read_size = 1;
+	// while (read_size != 0)
+	// {
+	read_size = read(fd, buf, BUFFER_SIZE);
+	if (read_size < 0)
+		ft_error("Failed to read file");
+	buf[read_size] = '\0';
+	// tmp = ft_strjoin(map, buf);
+	// ft_free(map);
+	// map = tmp;
+	// }
 	printf("[buf]: %zd\n%s\n", read_size, buf);
-	// printf("[map]\n%s\n", map);
 	ft_free(buf);
 	close(fd);
 	puts("close");
