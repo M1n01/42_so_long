5c48e4dcef2a322062fdb2ec31c1f005275e9600
Minato Abe <minato.cat.love@gmail.com>
2023-05-05 22:40:51 +0900

    add mlx

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:00:26 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/04 01:14:04 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/05 22:40:44 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include "libft.h"
-// # include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx.h"
 // # include "../minilibx-linux/mlx_int.h"
 // #include "debug.h"
 
