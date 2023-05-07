f054a38419f963032f20d10e17cfa06b39db7423
Minato Abe <minato.cat.love@gmail.com>
2023-05-07 23:02:46 +0900

    add destroy window

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:31:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/07 23:01:44 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/07 23:02:43 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,12 +94,12 @@ void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-// int	close(int keycode, t_mlx *mlx)
-// {
-// 	(void)keycode;
-// 	mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
-// 	return (0);
-// }
+int	ft_close(int keycode, t_mlx *mlx)
+{
+	(void)keycode;
+	mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
+	return (0);
+}
 
 void	mlx(char *map)
 {
@@ -119,7 +119,7 @@ void	mlx(char *map)
 	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	// ft_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 	// mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, img.img, 0, 0);
-	mlx_hook(mlx->win_ptr, 2, 1L<<0, close, mlx);
+	mlx_hook(mlx->win_ptr, 2, 1L<<0, ft_close, mlx);
 	mlx_loop(mlx->mlx_ptr);
 	// 最初のうちはCtrl+Cで終了
 	// のちに終了するプログラムを書く
