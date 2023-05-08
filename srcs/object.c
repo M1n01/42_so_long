6b06e9531a2c3c65605b8d2525f7c3a1abb4369c
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 15:26:48 +0900

    add obj

--- srcs/object.c
+++ srcs/object.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:51:53 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/08 15:25:58 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 15:26:44 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ t_obj	*init_obj(char *relative_path)
 	if (!obj)
 		ft_error("Malloc failed");
 	obj->img = NULL;
-	obj->width = 0;
-	obj->height = 0;
+	obj->width = NULL;
+	obj->height = NULL;
 	obj->path = relative_path;
 	return (obj);
 }
