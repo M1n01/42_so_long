11468d2f000ad1fcbd1b6fd2cb61b7d1cf1dfdf8
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 15:33:11 +0900

    add obj

--- srcs/object.c
+++ srcs/object.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:51:53 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/08 15:26:44 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 15:27:07 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ t_obj	*init_obj(char *relative_path)
 	if (!obj)
 		ft_error("Malloc failed");
 	obj->img = NULL;
-	obj->width = NULL;
-	obj->height = NULL;
+	obj->width = 0;
+	obj->height = 0;
 	obj->path = relative_path;
 	return (obj);
 }
@@ -29,6 +29,6 @@ t_obj	*init_obj(char *relative_path)
 void	put_obj(t_mlx *mlx, t_obj *obj, int x, int y)
 {
 	obj->img = mlx_xpm_file_to_image(mlx->mlx_ptr, obj->path,\
-		obj->width, obj->height);
+		&obj->width, &obj->height);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, obj->img, x, y);
 }
