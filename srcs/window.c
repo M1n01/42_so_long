22c2b795baf879d504cebe5d0a8215b369b423ca
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 18:33:17 +0900

    change t_game

--- srcs/window.c
+++ srcs/window.c
@@ -6,15 +6,15 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 23:26:00 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/07 23:26:19 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 18:33:15 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/so_long.h"
 
-int	win_close(int keycode, t_mlx *mlx)
+int	win_close(int keycode, t_game *game)
 {
 	if (keycode == ESC)
-		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx_destroy_window(game->ptr, game->win_ptr);
 	return (0);
 }
