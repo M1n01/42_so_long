e63e72c1a0e7561ea3af0607c4ec5faa8a9d169d
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 20:14:41 +0900

    put map

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -6,93 +6,58 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:31:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/08 20:09:39 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 20:14:32 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/so_long.h"
 
-// int	deal_key(int key, void *param)
-// {
-// 	// ft_putchar('X');
-// 	printf("%d\n", key);
-// 	(void)param;
-// 	return (0);
-// }
+int	deal_key(int key, void *param)
+{
+	// ft_putchar('X');
+	printf("%d\n", key);
+	(void)param;
+	return (0);
+}
 
-// void	draw_obj(t_mlx *mlx, int x, int y, int color)
-// {
-// 	int	i;
-// 	int	j;
+// map
+void	draw_map(char *map, t_game *game)
+{
+	size_t	i;
+	size_t	j;
+	size_t	width;
+	size_t	height;
 
-// 	i = 0;
-// 	while (i < 1)
-// 	{
-// 		j = 0;
-// 		while (j < 1)
-// 		{
-// 			mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, x + i, y + j, color);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
-
-// // map
-// void	draw_map(char *map, t_mlx *mlx)
-// {
-// 	size_t	i;
-// 	size_t	j;
-// 	size_t	width;
-// 	size_t	height;
-
-// 	width = count_map_width(map);
-// 	height = count_map_height(map);
-// 	i = 0;
-// 	while (i < height)
-// 	{
-// 		j = 0;
-// 		while (j < width)
-// 		{
-// 			if (map[i * width + j] == '1')
-// 			{
-// 				draw_obj(mlx, j, i, 0xFFFFFF);
-// 				puts("wall");
-// 			}
-// 			else if (map[i * width + j] == '0')
-// 			{
-// 				draw_obj(mlx, j, i, 0x000000);
-// 				puts("wall");
-// 			}
-// 			else if (map[i * width + j] == 'P')
-// 			{
-// 				draw_obj(mlx, j, i, 0xFF0000);
-// 				puts("wall");
-// 			}
-// 			else if (map[i * width + j] == 'C')
-// 			{
-// 				draw_obj(mlx, j, i, 0x00FF00);
-// 				puts("wall");
-// 			}
-// 			else if (map[i * width + j] == 'E')
-// 			{
-// 				draw_obj(mlx, j, i, 0x0000FF);
-// 				puts("wall");
-// 			}
-// 			mlx_key_hook(mlx->win_ptr, deal_key, NULL);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
-
-// void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
-// {
-// 	char	*dst;
-
-// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-// 	*(unsigned int *)dst = color;
-// }
+	width = count_map_width(map);
+	height = count_map_height(map);
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			if (map[i * width + j] == '1')
+			{
+				mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.wall, 0, 0);
+			}
+			else if (map[i * width + j] == 'P')
+			{
+				mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.player, 10, 10);
+			}
+			else if (map[i * width + j] == 'C')
+			{
+				mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.collectible, 50, 100);
+			}
+			else if (map[i * width + j] == 'E')
+			{
+				mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.exit, 100, 500);
+			}
+			mlx_key_hook(game->win_ptr, deal_key, NULL);
+			j++;
+		}
+		i++;
+	}
+}
 
 t_game	*init_game(char *map)
 {
@@ -117,10 +82,6 @@ void	newgame(char *map)
 	(void)map;
 	game = init_game(map);
 	game->win_ptr = mlx_new_window(game->ptr, game->width, game->height, "Game");
-	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.wall, 0, 0);
-	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.player, 10, 10);
-	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.collectible, 50, 100);
-	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.exit, 100, 500);
 	mlx_hook(game->win_ptr, 2, 1L<<0, win_close, game);
 	mlx_loop(game->ptr);
 	destroy_objs(game);
