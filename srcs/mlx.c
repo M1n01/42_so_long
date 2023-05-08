16c2712d76595a331d4e5cae0a814683b8758267
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 15:37:13 +0900

    add obj

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:31:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/08 15:36:56 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 15:37:10 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ void	mlx(char *map)
 		ft_error("Malloc failed");
 	init_mlx(mlx);
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, mlx->width, mlx->height, "test");
-	jiki = init_obj("./img/sprites/Pac-Man/pac_closed.xpm");
+	// jiki = init_obj("./img/sprites/Pac-Man/pac_closed.xpm");
 	wall = init_obj("./img/sprites/Walls/wall.xpm");
 	// exit = init_obj("./img/sprites/Portal/portal.xpm");
 	// coin = init_obj("./img/sprites/Pacdots/pacdot_powerup.xpm");
