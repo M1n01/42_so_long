7f78284ee7c620a0a9c05dafdbd0f0e93e9ea3d8
Minato Abe <minato.cat.love@gmail.com>
2023-05-08 20:07:01 +0900

    put wall

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:31:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/08 20:05:32 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/08 20:06:58 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ void	newgame(char *map)
 	(void)map;
 	game = init_game(map);
 	game->win_ptr = mlx_new_window(game->ptr, game->width, game->height, "Game");
-	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.wall, 0, 0);
+	mlx_put_image_to_window(game->ptr, game->win_ptr, game->objs.player, 0, 0);
 	mlx_hook(game->win_ptr, 2, 1L<<0, win_close, game);
 	mlx_loop(game->ptr);
 	destroy_objs(game);
