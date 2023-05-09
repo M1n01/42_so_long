ddf9d7dabd27e7828721a29bc24b08dada0c0c65
Minato Abe <minato.cat.love@gmail.com>
2023-05-09 10:08:43 +0900

    put map

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:31:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/09 10:05:45 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/09 10:08:40 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	draw_map(char *map, t_game *game)
 	while (i < height)
 	{
 		j = 0;
-		while (j <= width || map[i * width + j] != '\0')
+		while (j <= width && map[i * width + j] != '\0')
 		{
 			printf("%zd\n", i * width + j);
 			printf("%c\n", map[i * width + j]);
