cd0120c62e0de3011297159d6080975b929148b1
Minato Abe <minato.cat.love@gmail.com>
2023-05-10 15:48:18 +0900

    move player

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 11:23:36 by minabe            #+#    #+#             */
-/*   Updated: 2023/05/10 15:23:03 by minabe           ###   ########.fr       */
+/*   Updated: 2023/05/10 15:48:15 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,8 @@ void	move(t_game *game, int dir)
 		tmp_x += 1;
 		puts("right");
 	}
-	if (game->map[tmp_y * count_map_width(game->map) + tmp_x] != '1')
-	{
+	// if (game->map[tmp_y * count_map_width(game->map) + tmp_x] != '1')
+	// {
 		game->player.pre_x = game->player.x;
 		game->player.pre_y = game->player.y;
 		game->player.x = tmp_x;
@@ -48,7 +48,7 @@ void	move(t_game *game, int dir)
 		// game->redraw = 1;
 		game->turn++;
 		printf("turn: %ld\n", game->turn);
-	}
+	// }
 	printf("x: %d, y: %d\n", game->player.x, game->player.y);
 }
 
